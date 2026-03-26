a11c99c5431bf9021e79d7991a71424639f0481c
AsifNawaz-cnic <asif.nawaz@centralnic.com>
2026-03-26 16:52:21 +0100

    chore(publishing): update upload scripts to use API tokens for authentication

--- scripts/uploaddistribution_test.sh
+++ scripts/uploaddistribution_test.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # This script uploads the distribution packages to the Test PyPI repository.
-# It uses the environment variables TWINE_TEST_USERNAME and TWINE_TEST_PASSWORD for authentication.
+# It uses the environment variables TWINE_TEST_USERNAME and TWINE_TEST_TOKEN for authentication.
 
 # TWINE_TEST_USERNAME: The username for Test PyPI.
-# TWINE_TEST_PASSWORD: The password for Test PyPI.
+# TWINE_TEST_TOKEN: The password for Test PyPI.
+
+# no user name is needed when using API tokens, so we can skip the username and just use the token for authentication.
 
 # Increment the version number in your setup.py or pyproject.toml file before running this script.
 # Example for setup.py: update the version parameter in the setup() function.
 # Example for pyproject.toml: update the version field under [tool.poetry].
 
-twine upload -u "${TWINE_TEST_USERNAME}" -p "${TWINE_TEST_PASSWORD}" --repository-url https://test.pypi.org/legacy/ dist/* --skip-existing
+twine upload -p "${TWINE_TEST_TOKEN}" --repository-url https://test.pypi.org/legacy/ dist/* --skip-existing
